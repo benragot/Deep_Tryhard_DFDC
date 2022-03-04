27c2fd0aae8585d87b18f064125d0842b5f9a764
jeanbaptiste bordenave <jean_baptiste.bordenave@edu.escp.eu>
2022-03-04 15:28:54 +0100

    added the Yolov2 model + debug in utils

--- Deep_Tryhard_DFDC/utils.py
+++ Deep_Tryhard_DFDC/utils.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-def move_test_files(data_file,path = None, fake = 'fake',real = 'real', x_fakes = 0.2, x_reals = 0.2):
+def move_test_files(data_file,path = '', fake = 'fake',real = 'real', x_fakes = 0.2, x_reals = 0.2):
     '''
     Function to split our train data into x% of test data.
 
@@ -14,24 +14,32 @@ def move_test_files(data_file,path = None, fake = 'fake',real = 'real', x_fakes 
     8==============D
 
     '''
-    assert(data_file in os.listdir(path))
-    os.mkdir(data_file + '_test') # Please don't end train folder name with train or TV
-    fakes_test_path = os.path.join(data_file + '_test','fake_test')
-    reals_test_path = os.path.join(data_file + '_test','real_test')
-    os.makedirs(fakes_test_path)
-    os.makedirs(reals_test_path)
-    fakes_path = os.path.join(data_file,fake)
-    reals_path = os.path.join(data_file,real)
+    fullpath = os.path.join(path,data_file)
+
+    # assert(data_file in os.listdir(path))
+    isExist = os.path.exists(fullpath + '_test')
+    if not isExist:
+        os.mkdir(fullpath + '_test') # Please don't end train folder name with train or TV
+    fakes_test_path = os.path.join(fullpath + '_test','fake_test')
+    reals_test_path = os.path.join(fullpath + '_test','real_test')
+    isExist2 = os.path.exists(fakes_test_path)
+    if not isExist2:
+        os.makedirs(fakes_test_path)
+    isExist3 = os.path.exists(reals_test_path)
+    if not isExist3:
+        os.makedirs(reals_test_path)
+    fakes_path = os.path.join(fullpath,fake)
+    reals_path = os.path.join(fullpath,real)
     fake_list = os.listdir(fakes_path)
     real_list = os.listdir(reals_path)
 
     for i in range(int(len(fake_list)*x_fakes)):
-        ffile = fake_list.pop(random.randint(0,len(fake_list)-i))
+        ffile = fake_list.pop(random.randint(0,len(fake_list)-1 -i))
         os.replace(os.path.join(fakes_path,ffile),os.path.join(fakes_test_path,ffile))
 
     for i in range(int(len(real_list)*x_reals)):
 
-        rfile = real_list.pop(random.randint(0,len(real_list)-i))
+        rfile = real_list.pop(random.randint(0,len(real_list)-1 -i))
         os.replace(os.path.join(reals_path,rfile),os.path.join(reals_test_path,rfile))
 
 # Packages
